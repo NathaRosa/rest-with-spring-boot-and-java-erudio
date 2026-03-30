2333911782acb2db0c2afefc803ebe74b770bcca
NathaRosa <natharogerio123@gmail.com>
2026-03-30 07:04:50 -0400

    First Steps in Java with Spring Boot 4.0.4

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/natharosa/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.natharosa.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/natharosa/RestWithSpringBootAndJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.natharosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
