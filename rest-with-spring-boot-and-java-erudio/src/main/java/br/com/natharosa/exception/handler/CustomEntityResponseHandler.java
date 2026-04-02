c2a1680be1a863f9fc5603565f697738a12393fe
NathaRosa <natharogerio123@gmail.com>
2026-04-01 20:32:15 -0400

    Update modifications

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/natharosa/exception/handler/CustomEntityResponseHandler.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/natharosa/exception/handler/CustomEntityResponseHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @RestControllerAdvice
 public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler {
--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/natharosa/model/Greeting.java
+++
@@ -1,4 +0,0 @@
-package br.com.natharosa.model;
-
-public record Greeting(long id, String content) {
-}
