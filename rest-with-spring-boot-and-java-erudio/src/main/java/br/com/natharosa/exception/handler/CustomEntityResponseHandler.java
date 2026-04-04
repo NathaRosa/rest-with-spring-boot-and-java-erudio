ef2410fe76d4757d6d07af13efcf67dcc0eee630
NathaRosa <natharogerio123@gmail.com>
2026-04-04 09:37:11 -0400

    Changes and updates

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/natharosa/exception/handler/CustomEntityResponseHandler.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/natharosa/exception/handler/CustomEntityResponseHandler.java
@@ -1,7 +1,7 @@
 package br.com.natharosa.exception.handler;
 
 import br.com.natharosa.exception.ExceptionResponse;
-import br.com.natharosa.exception.UnsupportedMathOperationException;
+import br.com.natharosa.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -24,13 +24,13 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request) {
         ExceptionResponse response = new ExceptionResponse(
                 LocalDateTime.now(),
                 ex.getMessage(),
                 request.getDescription(false));
 
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
